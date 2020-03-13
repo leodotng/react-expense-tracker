90b69045ee329b4ee70c1db48ce05ce5c24eb6e2
leodotng <josh@leo.ng>
2020-03-13 11:02:04 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ marginTop: 20 }}>
+      <Button color="success">Let's start</Button>
+    </Container>
   );
 }
 
